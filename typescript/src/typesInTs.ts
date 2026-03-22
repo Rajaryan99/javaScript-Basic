f9fc648a2870dbeca6d98bc1359405902ff6a343
Raj/aryan <raj.aryan2518@gmail.com>
2026-03-22 22:59:59 +0530

    type inference and type script

---
+++ typescript/src/typesInTs.ts
@@ -0,0 +1,7 @@
+let drink = "coffee"
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let name = "raj Aryan" //“Type inference in TypeScript is the ability of the compiler to automatically determine the type of a variable based on its value, reducing the need for explicit type annotations.”
+
+let whichCoffee:string = "black coffee || Expresso without milk "
