972195b14d54a0a069a2a663bea51b638662c95d
Raj/aryan <raj.aryan2518@gmail.com>
2026-03-23 22:22:28 +0530

    union and any and undefined

--- typescript/src/union&Any.ts
+++ typescript/src/union&Any.ts
@@ -3,3 +3,19 @@ let subs: number | string = '1M'
 let apiRequestStatus: 'pending' | 'success' | 'error' = 'pending'
 
 apiRequestStatus = 'success'
+
+
+let orders = ["28", '12', '77', '69']
+
+let currentorder: string | undefined;
+
+for (let order of orders) {
+    if (order === '69') {
+        currentorder = order
+        break
+    }
+
+    currentorder = '11'
+}
+
+console.log(currentorder)
