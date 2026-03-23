c2ce24ffc148cc31788c3d5d21f824eaacc4e68f
Raj/aryan <raj.aryan2518@gmail.com>
2026-03-23 21:33:27 +0530

    custome type in typescript

--- typescript/src/typesInTs.ts
+++ typescript/src/typesInTs.ts
@@ -4,4 +4,5 @@ let cups = Math.random() > 0.5 ? 10 : 5;
 
 let name = "raj Aryan" //“Type inference in TypeScript is the ability of the compiler to automatically determine the type of a variable based on its value, reducing the need for explicit type annotations.”
 
-let whichCoffee:string = "black coffee || Expresso without milk "
+let whichCoffee: string = "black coffee || Expresso without milk "
+
