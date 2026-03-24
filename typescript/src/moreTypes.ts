2ef2979585234811378eb3d0dac02e2098378c53
Raj/aryan <raj.aryan2518@gmail.com>
2026-03-24 14:10:38 +0530

    never type

--- typescript/src/moreTypes.ts
+++ typescript/src/moreTypes.ts
@@ -3,3 +3,19 @@ let response: any = '45'
 let numbricLenght: number = (response as string).length
 
 console.log(numbricLenght)
+ 
+type Role = "admin" | "user" | 'superAdmin';
+
+
+function redirectBasedOnRole(role: Role): void {
+    if (role === "admin") {
+        console.log("Redirecting to admin dashboard")
+        return
+    }
+
+    if (role === "user") {
+        console.log("Redirecting to user Dashboard")
+        return
+    }
+    role;
+}
