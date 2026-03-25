5b73cc9e7efc3333b92fbb1f7e71171f6c443e29
Raj/aryan <raj.aryan2518@gmail.com>
2026-03-25 15:55:41 +0530

    readobly property in typescript

--- typescript/src/arrayTS.ts
+++ typescript/src/arrayTS.ts
@@ -1,5 +1,16 @@
 const chaiFlavours: string[] = ['masala', 'lemon']
 const chaiPrice: number[] = [10, 20]
 
-const ratng: Array<number> =  [4.5, 5.5]
+const ratng: Array<number> = [4.5, 5.5]
+
+
+
+const menu: chai[] = [
+    { name: 'masala', price: 15 },
+    {name: 'adrak', price: 25}
+]
+
+
+
+const chities: readonly string[] = ['Delhi', 'mumbai']
 
