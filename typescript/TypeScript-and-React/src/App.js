3ebdd90d53f00098ac0895eb8721a83df48f7065
Raj/aryan <raj.aryan2518@gmail.com>
2026-03-27 02:41:38 +0530

    first components in TS

---
+++ typescript/TypeScript-and-React/src/App.js
@@ -0,0 +1,8 @@
+import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
+import './App.css';
+import Button from './components/Button';
+function App() {
+    return (_jsx(_Fragment, { children: _jsx(Button, {}) }));
+}
+export default App;
+//# sourceMappingURL=App.js.map
