0e172b8575c564262fb17445d4639b1fd99c8f5a
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-04 15:00:09 +0530

    slice function

--- Basic/string.js
+++ Basic/string.js
@@ -1,11 +1,16 @@
- const  name = "raj"
+ const  name = "rajaryan"
  const repoCount = 50;
 
 //  console.log(name + repoCount + " value")
 
-console.log(`Hello my ${name} is repo count is ${repoCount}`)
+// console.log(`Hello my ${name} is repo count is ${repoCount}`)
 
 
-console.log(name.toUpperCase())
-console.log(name.charAt(2))
-console.log(name.indexOf('r'))
+// console.log(name.toUpperCase())
+// console.log(name.charAt(2))
+// console.log(name.indexOf('r'))
+
+const newString = name.slice(-7, 4)
+// const newString = name.substring(0,2)
+
+console.log(newString)
