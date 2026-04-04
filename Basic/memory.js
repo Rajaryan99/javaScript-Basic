693df4006c87a331b1063ce96cd920974cdba05c
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-04 14:35:17 +0530

    refference

--- Basic/memory.js
+++ Basic/memory.js
@@ -1,3 +1,29 @@
 // Heap memory and Stack Memory
 
 // Heap (Non- Primitive) and Stack is a (Primitive) data type
+
+let youtubrName = "rajaryan"
+
+let anotherName = youtubrName
+
+anotherName = "coffee and code "
+console.log(anotherName)
+console.log(youtubrName)
+
+
+let userOne = {
+    email: "user@yahoo.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+console.log(userTwo)
+
+userTwo.email = "joan@yahoo.com"
+
+
+console.log(userTwo)
+
+
+
