6fb1cd86bae2b46efd55ba9262b83cea43e379aa
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-04 14:46:32 +0530

    string

---
+++ Basic/string.js
@@ -0,0 +1,9 @@
+ const  name = "raj"
+ const repoCount = 50;
+
+//  console.log(name + repoCount + " value")
+
+console.log(`Hello my ${name} is repo count is ${repoCount}`)
+
+
+console.log(name.toUpperCase())
