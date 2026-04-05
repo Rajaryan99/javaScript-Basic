727ac0912a148f5c6cddce81c1bdee03e560b6a1
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-05 13:32:36 +0530

    splice and slice

--- Basic/Array.js
+++ Basic/Array.js
@@ -6,10 +6,28 @@ console.log(myArrry[3])
 
 // myArrry.push(4)
 // console.log(myArrry)
-// myArrry.pop()
+// // myArrry.pop()
+// // console.log(myArrry)
+
+// const newArr = myArrry.join()
+
 // console.log(myArrry)
+// console.log( typeof newArr)
 
-const newArr = myArrry.join()
 
-console.log(myArrry)
-console.log( typeof newArr)
+//slice and Splice
+
+const arr  =  [0,1,2,3,4]
+console.log("A", arr)
+
+const newArr  = arr.slice(1, 3) //original array does not chnage
+console.log(newArr);
+console.log("B", arr)
+
+
+const newArr1 = arr.splice(1,3) // orifian array chnage
+console.log(newArr1)
+console.log("c", arr)
+
+
+
