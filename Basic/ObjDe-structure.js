6c8fef1ae38a1774dfaacc163a806dafcac670c4
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-06 01:59:08 +0530

    destructre in the object

---
+++ Basic/ObjDe-structure.js
@@ -0,0 +1,13 @@
+// Object De-Structure and JSON API Introduction
+
+const course  = {
+    name: 'made easy with javascript',
+    price: '999',
+    Authore: "Raj arayn Singh"
+}
+
+// course
+
+const {name, price, Authore} = course;
+
+console.log(Authore)
