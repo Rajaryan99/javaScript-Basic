1deda7a59340cb6260f0c6b9ee4cc99de4ddf278
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-06 00:04:11 +0530

    object combining

--- Basic/Object2.js
+++ Basic/Object2.js
@@ -8,3 +8,9 @@ tinderUser.isLogedIn = false
 
 // console.log(tinderUser)
 
+const obj1 = {1: 'a', 2: 'b'}
+const obj2 = {3: 'a', 4: 'b'}
+
+const obj3 = Object.assign({}, obj1, obj2)
+
+console.log(obj3)
