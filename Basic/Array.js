1725483676f2f41543481c85dbb92e8698c8a1a0
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-05 13:16:19 +0530

    arrays method

---
+++ Basic/Array.js
@@ -0,0 +1,15 @@
+// Array
+
+
+const myArrry = [1,2,3,4]
+console.log(myArrry[3])
+
+// myArrry.push(4)
+// console.log(myArrry)
+// myArrry.pop()
+// console.log(myArrry)
+
+const newArr = myArrry.join()
+
+console.log(myArrry)
+console.log( typeof newArr)
