ed0c23381273993c39cf542514e3a88c4074f303
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-07 00:36:05 +0530

    arrow funtion

--- Basic/ArrowFunction.js
+++ Basic/ArrowFunction.js
@@ -4,10 +4,27 @@ const user = {
     username: "Raj Aryan",
     price: 777,
 
-    welcomeMessage: function(){
+    welcomeMessage: function(){ // this reffers to current context.
         console.log(`Hello ${this.username}, welcome to our website` )
+        console.log(this);
+        
     }
 
 }
+// user.username = "sakshi"
+// user.welcomeMessage()
 
-console.log(user.welcomeMessage())
+// console.log(this)
+
+
+function coffee(){
+    console.log(this)
+}
+// coffee()
+
+
+const chai = () => {
+    console.log(this)
+}
+
+chai()
