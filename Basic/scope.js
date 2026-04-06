546850946c72bf93491f4d90716207c296f260ba
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-06 18:13:07 +0530

    clousers in js

---
+++ Basic/scope.js
@@ -0,0 +1,14 @@
+function one(){
+    const username= "RajAryan"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+
+    // console.log(website)
+
+    // two() // clouser the inner function can access the out function variable
+}
+
+one()
