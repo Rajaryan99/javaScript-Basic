a251bb1ebba32d416512f512e71a322a766ba2d0
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-07 14:54:48 +0530

    map and key values

--- Basic/highOrderFunction.js
+++ Basic/highOrderFunction.js
@@ -3,11 +3,25 @@
 const arr = [1,2,3,4,5]
 
 for (const num of arr) {
-    console.log(num);
+    // console.log(num);
 }
 
 const greeting = "Hello Wolrd!"
 for(const greet of greeting){
     if(greet == " ") continue;
-    console.log(`Each char is ${greet}`)
+    // console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+
+map.set('IN', "India")
+map.set('USA', "united states")
+map.set('EU', "Europe")
+
+// console.log(map)
+
+for(const [key, value] of map){
+    console.log(`short is ${key} and full name is ${value}`)
 }
