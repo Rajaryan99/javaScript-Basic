da0bf4ebe7c7419d654d4d3a7cd9eba6a804d7eb
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-09 15:06:35 +0530

    filter map

--- Basic/methods.js
+++ Basic/methods.js
@@ -14,7 +14,7 @@ const coding  = ['js', 'ruby', 'java', 'python', 'cpp']
 const nums = [1, 2, 3, 4, 5, 6];
 
 
-const newNums = nums.filter((num) => (
+const newNum = nums.filter((num) => (
      num > 4
     // return greateThen4
     
@@ -44,6 +44,25 @@ let userBooks = books.filter((bk) => bk.genre.toLowerCase() === "history"
 userBooks = books.filter((bk) => bk.publish > 2000 &&   bk.genre.toLowerCase() === "history" )
 
 
-console.log(userBooks)
+// console.log(userBooks)
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const filterNums = myNums.filter((num) => num > 5)
+// console.log(myNums)
+console.log(filterNums)
+console.log(myNums)
+
+
+console.log("----------------------------------------------------------------------------")
+
+let newNums = myNums.map((num) => num + 10)
+
+console.log(newNums)
+console.log(myNums)
+
+
+
 
 
