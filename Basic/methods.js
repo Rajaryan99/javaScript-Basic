800c661e7fd879145bd7d7617c8c4a13d33c81ba
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-09 13:54:40 +0530

    filter in array

---
+++ Basic/methods.js
@@ -0,0 +1,25 @@
+// Methods like Filter, reduce and map 
+
+const coding  = ['js', 'ruby', 'java', 'python', 'cpp']
+
+
+// const value = coding.forEach((i) => {
+//     // console.log(i)
+//     return i
+// })
+
+// console.log(value)
+
+
+const nums = [1, 2, 3, 4, 5, 6];
+
+
+const newNums = nums.filter((num) => {
+    const greateThen4 = num > 4
+    return greateThen4
+    
+})
+
+console.log(newNums)
+
+
