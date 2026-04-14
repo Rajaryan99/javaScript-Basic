997e00a0eaa6969927df8d74eb6f2e5bc954b849
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-14 12:17:23 +0530

    third image

--- Basic/events.js
+++ Basic/events.js
@@ -5,3 +5,7 @@ const secImg = document.querySelector('#second');
 secImg.addEventListener('click', (e) => {
     alert("HEY, THIS IS SECOND IMAGE")
 })
+
+secImg.addEventListener('click', (e) => {
+    alert("Hey, this is 3rd image")
+})
