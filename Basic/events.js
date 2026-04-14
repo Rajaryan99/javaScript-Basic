f1e0424ed5141b00052376d80723a2c0f68bcb78
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-14 12:15:51 +0530

    envents in javascript

---
+++ Basic/events.js
@@ -0,0 +1,7 @@
+// |------------------------ Events in javascripts ------------------------------|
+
+const secImg = document.querySelector('#second');
+
+secImg.addEventListener('click', (e) => {
+    alert("HEY, THIS IS SECOND IMAGE")
+})
