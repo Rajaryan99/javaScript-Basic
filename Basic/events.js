597fadd82ead8901d97ab0bd52da11f99be66c45
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-17 23:16:44 +0530

    remove parent Node

--- Basic/events.js
+++ Basic/events.js
@@ -1,23 +1,32 @@
 // |------------------------ Events in javascripts ------------------------------|
 
 const secImg = document.querySelector('#second');
+let img = document.getElementById('images')
 
-secImg.addEventListener('click', (e) => {
-    alert("HEY, THIS IS SECOND IMAGE")
-})
-
-
- document.getElementById('third').addEventListener('click', (e) => {
-    e.preventDefault()
-        alert("Clicked the 3rd project")
-    }, true)
+// secImg.addEventListener('click', (e) => {
+//     alert("HEY, THIS IS SECOND IMAGE")
+// })
 
 
-    document.getElementById('images').addEventListener('click', (e) => {
-        console.log('elements inside UL was clicked!!')
-    })
+//  document.getElementById('third').addEventListener('click', (e) => {
+//     e.preventDefault()
+//         alert("Clicked the 3rd project")
+//     }, true)
 
-    document.getElementById('fourth').addEventListener('click', (e) => {
-        console.log('fourth image was clicked')
-        e.stopPropagation()
+
+//     document.getElementById('images').addEventListener('click', (e) => {
+//         console.log('elements inside UL was clicked!!')
+//     })
+
+//     document.getElementById('fourth').addEventListener('click', (e) => {
+//         console.log('fourth image was clicked')
+//         e.stopPropagation()
+//     })
+
+
+
+    img.addEventListener('click', (e) => {
+            console.log(e.target.parentNode)
+            let removeIt = e.target.parentNode
+            removeIt.remove();
     })
