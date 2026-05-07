b52f35b34b6f335992d24f0b50aeeeacf009b6e0
Raj Aryan <raj.aryan2518@gmail.com>
2026-05-07 13:51:24 +0530

    semd data to mail usimg fetch post methd

--- basicJS/Form/src/App.jsx
+++ basicJS/Form/src/App.jsx
@@ -5,14 +5,51 @@ import './App.css'
 function App() {
 
   const [form, setForm] = useState({name:"", email:"", message:""})
-  console.log(form);
+  const [isSending, setIsSending] = useState(false)
+  //console.log(form);
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
   console.log("Name:", form.name)
   console.log("email:", form.email)
   console.log("message:", form.message)
+
+    if(isSending) return;
+
+    const formData = new FormData(e.currentTarget)
+
+
+  setIsSending(true)
+
+  try {
+    
+    const response  = await fetch("https://formsubmit.co/ajax/singhrajaryan66@gmail.com", {
+
+    method: "POST",
+    headers: {
+      Accept: "application/json"
+    },
+    body: formData,
+    //body: JSON.stringify(form),
+  })
+
+
+  if(!response.ok){
+    console.log('something is wrong')
+  }
+
+  if(response.ok){
+    console.log("Message is send the mail")
+  }
+  } catch (error) {
+    console.log(error, "error in semd mail")
+    
+  } finally{
+    setIsSending(false)
+  }
+
+  
 
   setForm({
     name: "",
@@ -58,7 +95,9 @@ function App() {
         onChange={(e) => setForm({...form, message: e.target.value})}
         placeholder='hello.....' />
         </div>
-        <button type='submit' >Submit</button>
+        <button type='submit' >
+          {isSending ? "sending" : "send message"}
+        </button>
       </form>
     </>
   )
